dbcd54611a34a31f45eae0f43308d81c2ab5b53a
paweu9000 <wolfenorrin5@gmail.com>
2024-04-08 10:48:08 +0200

    initial concentration module

--- test/ex_statistics_test.exs
+++ test/ex_statistics_test.exs
@@ -1,10 +1,11 @@
 defmodule ExStatisticsTest do
   use ExUnit.Case
-  alias ExStatistics.{Data, Quartiles, Coefficients, Asymmetry}
+  alias ExStatistics.{Data, Quartiles, Coefficients, Asymmetry, Concentration}
   doctest Data
   doctest Quartiles
   doctest Coefficients
   doctest Asymmetry
+  doctest Concentration
 
 
 end
